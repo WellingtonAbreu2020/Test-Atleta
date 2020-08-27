566fad51da776b15bce4b0e7f71829a3147123c9
WellingtonAbreu <wellington.abreu2020@outlook.com>
2020-08-27 15:18:56 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+  public static void main(String[] args) {
+   TesteSimples teste =new TesteSimples();
+   teste.executar();
+   System.out.println("imcmonitor, fim")
+  }
+}
---
+++ atleta.java
@@ -0,0 +1,21 @@
+class atleta{
+  private String nome;
+    private int idade;
+
+    public void setNome(String n){
+      nome=n;
+    }
+
+    public void setIdade(int i){
+      idade = i;
+       }
+        public String getnome(){
+          return nome;
+           }
+           public int getidade(){
+          return idade;
+           }
+
+
+  }
+}
